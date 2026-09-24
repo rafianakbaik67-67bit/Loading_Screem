6ba142bcf7a7c7b5a42f9229c5147315fab7b817
rafianakbaik67-67bit <rafianakbaik67@gmail.com>
2026-09-24 14:36:38 +0700

    Loading_screen step 1 to 5

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int TotalSteps = 10;
+
+for (int step = 1; step <= TotalSteps; step++)
+{
+    Console.WriteLine($"Loading..step{step}of{TotalSteps}");
+    if (step % 3 == 0)
+        Console.WriteLine("Checkpoint reached");
+}
+
+Console.WriteLine("\nDungeon is ready. Enter now!");
